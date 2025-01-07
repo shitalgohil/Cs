e92b697d799e5a9b85b5211b29a76209ead39ee4
Shital Gohil <155747453+shitalgohil@users.noreply.github.com>
2025-01-07 21:53:57 +0530

    Create pattern2

---
+++ pattern2.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
